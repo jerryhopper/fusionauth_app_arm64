5b69fbd0df269c9575299e5104d2f4beedd2db00
JerryHopper <hopper.jerry@gmail.com>
2020-06-30 12:11:52 +0200

    Update buildscript.sh
    added some minimal checks

--- buildscript.sh
+++ buildscript.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-
+# grab latest version
 REMOTEVERSION="$(curl -s 'https://hub.docker.com/v2/repositories/fusionauth/fusionauth-app/tags/?page_size=25&page=1' | jq -r '.results[1].name')"
 
-if [ ! -f /tmp/fusionauth.build.version ]; then
+# if no local version available, set one.
+if [ ! -f /root/fusionauth.build.version ]; then
   REMOTEVERSION="1.17.2"
 else
-  LOCALVERSION="$(</tmp/fusionauth.build.version)"
+  LOCALVERSION="$(</root/fusionauth.build.version)"
 fi
 
 
-
+# VersionCheck
 if [ "$REMOTEVERSION" == "$LOCALVERSION" ] ; then
   echo "remote ($REMOTEVERSION) local ($LOCALVERSION) equal."
   exit
@@ -19,24 +20,45 @@ else
   echo "remote ($REMOTEVERSION) & local ($LOCALVERSION) not equal."
 fi
 
+# Clean the local docker image, so we have everything fresh.
 docker system prune -a -f
 
+# Build the container from git
 docker build --build-arg FUSIONAUTH_VERSION=$REMOTEVERSION https://github.com/jerryhopper/fusionauth_app_arm64.git -t jerryhopper/fusionauth-app-arm64:$REMOTEVERSION
 
-#docker tag fusionauth-app-arm64:latest jerryhopper/fusionauth-app-arm64:$REMOTEVERSION
+
+# Stop if fusionauth-app-test container is running.
+docker stop fusionauth-app-test
+# Remove if fusionauth-app-test container exists.
+docker rm  fusionauth-app-test
+# Run the container
+docker run -d -p 9011:9011 --name fusionauth-app-test jerryhopper/fusionauth-app-arm64:$REMOTEVERSION
+sleep 20
+
+# Check if FA is running.
+RES="$(curl -sL http://localhost:9011/api/status|grep 'title>Maintenance')"
+if [ "$RES" == "" ]; then
+  echo "test failed"
+  docker stop fusionauth-app-test
+  docker rm  fusionauth-app-test
+  exit
+fi
+
+#curl -sL http://localhost:9011/api/status|grep  'js?version='
+docker stop fusionauth-app-test
+docker rm  fusionauth-app-test
+
+
+
+# Push the container to dockerhub
 docker push jerryhopper/fusionauth-app-arm64:$REMOTEVERSION
 
+# Build again, but latest version.
 docker build --build-arg FUSIONAUTH_VERSION=$REMOTEVERSION https://github.com/jerryhopper/fusionauth_app_arm64.git -t jerryhopper/fusionauth-app-arm64:latest
-#docker tag fusionauth-app-arm64:$REMOTEVERSION jerryhopper/fusionauth-app-arm64:latest
+# Push the latest tag to dockerhub 
 docker push jerryhopper/fusionauth-app-arm64:latest
 
-echo "$REMOTEVERSION">/tmp/fusionauth.build.version
-
-
-
-
-
-
-
+# Write the version to local file.
+echo "$REMOTEVERSION">/root/fusionauth.build.version
 
 
